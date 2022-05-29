962fe165a4c228df5fb10d247d3ab16bf76cfa00
yudbox <a.yudbox@gmail.com>
2022-05-29 11:01:43 +0300

    reconfiguring terraform s3

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -99,6 +99,10 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
 
     effect = "Allow"
 
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
 
     actions = [
       "s3:GetObject"
