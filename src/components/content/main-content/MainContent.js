facf31cd9e9191adce521ddefd7b0efa8cf0f8a2
yudbox <a.yudbox@gmail.com>
2022-05-30 09:45:44 +0300

    pagination under the slider

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Paginate from "../paginate/Paginate";
 import SlideShow from "../slide-show/SlideShow";
 import "./MainContent.scss";
 
@@ -15,12 +16,23 @@ const images = [
 ];
 
 const MainContent = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === "prev" && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
   return (
     <div className="main-content">
-      <SlideShow images={images} auto={true} showArrows={true} />
+      <SlideShow images={images} auto={false} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movie-type">Now playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+        </div>
       </div>
     </div>
   );
