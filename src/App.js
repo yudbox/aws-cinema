1221188c9daa17165ec065940e04f8ecf53a4cbf
yudbox <a.yudbox@gmail.com>
2022-08-06 01:27:59 +0300

    error page fix conflicts 10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import Header from "./components/header/Header.js";
+import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Details from "./components/content/details/Details";
