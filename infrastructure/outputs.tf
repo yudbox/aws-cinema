cfedfcbd5a2c2e8641dc49f6d80c39a87f8a7d3a
yudbox <60258219+yudbox@users.noreply.github.com>
2022-08-06 23:13:52 +0300

    Feature/details (#27)
    * add preloader to details page
    
    * add preloader to details page 2
    
    * add preloader to details page 4
    
    * add preloader to details page 6

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,6 +7,6 @@ output "cinema_app_bucket_name" {
   value = aws_s3_bucket.cinema_app_s3_bucket.id
 }
 
-output "cloudfront_distribution_yudbox_id" {
-  value = aws_cloudfront_distribution.s3_cinema_app_distribution.id
-}
+# output "cloudfront_distribution_yudbox_id" {
+#   value = aws_cloudfront_distribution.s3_cinema_app_distribution.id
+# }
